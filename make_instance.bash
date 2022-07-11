8bd93b5cc6abb64a3e8bbb262d46f8df62d7764e
Alexander Pieper <alexanderpieper@alexandersmbp2.speedport.ip>
2022-07-11 18:35:19 +0200

    updates from the lecture

--- make_instance.bash
+++ make_instance.bash
@@ -1,2 +1,4 @@
-gen -x 9 -y 6 -X 3 -Y 2 -s 6 -p 1 -r 2 -H -P 4 -u 20 -o 4 --oap
+gen -x 9 -y 6 -X 5 -Y 10 -s 6 -p 1 -r 2 -H -P 4 -u 20 -o 4 --oap
+gen -x 20 -y 6 -X 5 -Y 10 -s 6 -p 1 -r 2 -H -P 4 -u 20 -o 4 --oap
+gen -x 30 -y 20 -X 5 -Y 10 -s 6 -p 1 -r 2 -H -P 4 -u 20 -o 4 --oap
 
